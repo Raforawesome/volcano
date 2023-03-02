27f5fce026dff198723ff0b5db52e0b4c89889d8
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-02 10:27:18 -0500

    Add note listing

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,22 @@
+#![allow(unused_assignments, dead_code)]
 use dioxus::prelude::*;
 
 pub fn app(cx: Scope) -> Element {
+	// let note_names: Vec<String> = crate::fs::get_note_names().unwrap();
+	let note_names: Vec<String> = vec![
+		"Unit 1 - Day 1".into(),
+		"Unit 1 - Day 2".into(),
+	];
 	cx.render(rsx!(
 		style { include_str!("./index.css") }
 		div {
-			class: "sidebar-div"
+			class: "sidebar-div",
+			note_names.iter().map(|s| rsx!(
+				button {
+					class: "note-name",
+					"{s}"
+				}
+			))
 		}
 	))
 }
