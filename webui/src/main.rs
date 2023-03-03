89fc6c2679ce3ed34fecef79e7017bdaeac2e9dd
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-02 21:49:56 -0500

    Reset `webui` to a blank crate

--- webui/src/main.rs
+++ webui/src/main.rs
@@ -1,9 +1,3 @@
-#![feature(try_blocks)]
-mod app;
-mod fs;
-
 fn main() {
-	// let note_names: Vec<String> = fs::get_note_names().unwrap();
-	dioxus_web::launch(app::app);
-	// dioxus_web::launch_with_props(app::app, app::appProps { note_names }, dioxus_web::Config::default());
+	println!("Hello, world!");
 }
