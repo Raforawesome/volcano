c537fe90bfed76ed2ee1436a2a50e74bc3d5911e
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-06 10:18:24 -0500

    Add SCSS build script

--- webui/build.rs
+++ webui/build.rs
@@ -1,4 +1,32 @@
 use rsass::{output::{Format, Style}};
+use std::fs;
+
+const CSS_DIR: &str = "src/css/";
 
 fn main() {
+	println!("cargo:rerun-if-changed={CSS_DIR}");
+	fs::read_dir(CSS_DIR)
+		.expect("Failed to read CSS directory: ")
+		.filter_map(Result::ok)
+		.filter(|obj| !obj.path().is_dir() 
+			&& obj.file_name().into_string().unwrap().ends_with(".scss"))
+		.for_each(|obj| {
+			let css: String = fs::read_to_string(obj.path()).unwrap();
+
+			let mut scss: String;
+			unsafe {
+				scss = String::from_utf8_unchecked(
+					rsass::compile_scss(css.as_bytes(), 
+					Format {
+						style: Style::Compressed,
+						..Default::default()
+					}
+					).unwrap()
+				);
+			}
+
+			fs::write(CSS_DIR.to_owned() + 
+				&obj.file_name().to_str().unwrap().replace(".scss", ".css"), &scss).unwrap();
+			println!("written to {}", obj.file_name().to_str().unwrap().replace(".scss", ".css"));
+		});
 }
