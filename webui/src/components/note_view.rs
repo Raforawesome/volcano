33f22f59cee9b3ff5060e24d0c32e4422ba9242b
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-07 14:06:30 -0500

    Add dynamic token parsing to `NoteView` component

--- webui/src/components/note_view.rs
+++ webui/src/components/note_view.rs
@@ -1,17 +1,36 @@
 use dioxus::prelude::*;
+use volcano_parser::TokenType;
 
 #[inline_props]
 #[allow(non_snake_case)]
 pub fn NoteView<'a>(cx: Scope, contents: &'a str) -> Element {
-	cx.render(
-		rsx!(
-			p {
-				"Running a "
-				b {
-					"bold"
-				}
-				" test"
-			}
-		)
-	)
+    let tokens = volcano_parser::tokenize_markdown(contents);
+
+    cx.render(rsx!(
+        p {
+            "Running a "
+            b {
+                "bold"
+            }
+            " test"
+        }
+
+        tokens.iter()
+        .filter(|t| {
+            (&contents[t.span.0..t.span.1]).trim() != ""
+        })
+        .map(|t| {
+            match t.ty {
+                TokenType::H1 => {
+                    let sub: &'a str = &contents[t.span.0..t.span.1];
+                    rsx!(
+                        h1 { "{sub}" }
+                    )
+                }
+                _ => rsx!(
+                    b { "Invalid token!" }
+                )
+            }
+        })
+    ))
 }
