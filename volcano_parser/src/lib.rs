8d4a32d0dca88b2281eb17641f7c669e6403d060
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-07 14:05:49 -0500

    Export new `TokenType` enum

--- volcano_parser/src/lib.rs
+++ volcano_parser/src/lib.rs
@@ -1,24 +1,24 @@
 //! # Volcano Markdown Parser
 //! **A blazingly fast, zero-allocation minimal markdown parser.**
-//! 
+//!
 //! ---
-//! 
+//!
 //! This parser **does not** aim to implement every markdown feature.
 //! It implements a minimal superset, based on what I use for my notes.
-//! Notably, this includes headers, bold text, italic text, and most 
+//! Notably, this includes headers, bold text, italic text, and most
 //! importantly LaTeX mode, with support for both inline ($expr$) and
 //! display ($$expr$$) modes.
 
 mod parse;
-pub use parse::{LatexType, ListType, MdToken, Span, tokenize_markdown};
+pub use parse::{tokenize_markdown, LatexType, ListType, MdToken, Span, TokenType};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-		let md: String = std::fs::read_to_string("md.txt").expect("Failed to find markdown file!");
-		dbg!(parse::tokenize_markdown(&md));
+    fn test_tokenizer() {
+        let md: String = std::fs::read_to_string("md.txt").expect("Failed to find markdown file!");
+        dbg!(parse::tokenize_markdown(&md));
     }
 }
