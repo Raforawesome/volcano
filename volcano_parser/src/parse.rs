6b654f22b24f0eba4d974ab08fecab07c2654d12
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-07 10:07:48 -0500

    Create token/parser types

---
+++ volcano_parser/src/parse.rs
@@ -0,0 +1,33 @@
+#![allow(unused)]
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Span(usize, usize);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum MdToken {
+	H1(Span),
+	H2(Span),
+	H3(Span),
+	H4(Span),
+	H5(Span),
+	Bold(Span),
+	Italic(Span),
+	Latex(LatexType, Span),
+	List(ListType, Vec<Span>)
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum LatexType {
+	Inline,
+	Display
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ListType {
+	Ord,
+	Unord
+}
+
+pub fn tokenize_markdown(md: &str) -> Vec<MdToken> {
+	todo!()
+}
