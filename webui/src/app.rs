b5bd8893d68b1a7110b6bceacea209288a8e41e0
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-07 14:06:14 -0500

    Add dummy temp file to main app file

--- webui/src/app.rs
+++ webui/src/app.rs
@@ -1,12 +1,25 @@
 use dioxus::prelude::*;
 use crate::components::note_view::NoteView;
 
+const TEST_FILE: &str = 
+r#"
+# Header 1
+## Header 2
+### Header 3
+
+**bold**
+Inline **bold** text
+
+*italic*
+Inline *italic* test
+"#;
+
 pub fn app(cx: Scope) -> Element {
 	cx.render(
 		rsx!(
 			style { include_str!("./css/index.css") }
 			h1 { "Hello, world!" }
-			NoteView { contents: "" }
+			NoteView { contents: TEST_FILE }
 		)
 	)
 }
