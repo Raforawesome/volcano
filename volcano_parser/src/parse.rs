be4ee181497d7c45313c957ec98d134c4bbbb9dd
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-07 14:06:00 -0500

    Seperate token type and Span variables

--- volcano_parser/src/parse.rs
+++ volcano_parser/src/parse.rs
@@ -1,33 +1,40 @@
 #![allow(unused)]
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Span(usize, usize);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Default)]
+pub struct Span(pub usize, pub usize);
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum MdToken {
-	H1(Span),
-	H2(Span),
-	H3(Span),
-	H4(Span),
-	H5(Span),
-	Bold(Span),
-	Italic(Span),
-	Latex(LatexType, Span),
-	List(ListType, Vec<Span>)
+pub enum TokenType {
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    Bold,
+    Italic,
+    Latex(LatexType, Span),
+    List(ListType, Vec<Span>),
+    Invalid,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct MdToken {
+    pub ty: TokenType,
+    pub span: Span,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum LatexType {
-	Inline,
-	Display
+    Inline,
+    Display,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ListType {
-	Ord,
-	Unord
+    Ord,
+    Unord,
 }
 
 pub fn tokenize_markdown(md: &str) -> Vec<MdToken> {
-	todo!()
+    todo!()
 }
