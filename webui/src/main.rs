25f4eeeaa00ae3447c48c425ffc52f77864d13e6
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-20 10:02:18 -0400

    Run auto-formatters

--- webui/src/main.rs
+++ webui/src/main.rs
@@ -9,6 +9,6 @@ mod app;
 mod components;
 
 fn main() {
-	wasm_logger::init(wasm_logger::Config::default());
+    wasm_logger::init(wasm_logger::Config::default());
     dioxus_web::launch(app::app);
 }
