d23af4110d9e6687224e9845261e0874e59a1800
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-20 09:59:36 -0400

    Wrap note view element in a div for easy rendering

--- webui/src/components/note_view.rs
+++ webui/src/components/note_view.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use dioxus_web::use_eval;
 use volcano_parser::{TokenType, TextType};
 #[allow(unused)]
 use log::{info, error};
@@ -8,83 +9,88 @@ use log::{info, error};
 pub fn NoteView<'a>(cx: Scope, contents: &'a str) -> Element {
     let tokens = volcano_parser::tokenize_markdown(contents);
 
-    cx.render(rsx!(tokens.iter().map(|t| {
-        match &t.ty {
-            TokenType::H1 => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    h1 { "{sub}" }
-                )
-            }
-            TokenType::H2 => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    h2 { "{sub}" }
-                )
-            }
-            TokenType::H3 => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    h3 { "{sub}" }
-                )
-            }
-            TokenType::H4 => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    h4 { "{sub}" }
-                )
-            }
-            TokenType::H5 => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    h5 { "{sub}" }
-                )
-            }
-            TokenType::Bold => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    b { "{sub}" }
-                )
-            }
-            TokenType::Italic => {
-                let sub: &'a str = &contents[t.span.0..t.span.1];
-                rsx!(
-                    i { "{sub}" }
-                )
-            }
-			TokenType::Inline(tokens) => {
-				info!("Found inline token");
-				rsx!(
-					p {
-						tokens.iter().map(|t| {
-							match t.ty {
-								TextType::Bold => {
-									rsx!(
-										b { &contents[t.span.0..t.span.1] },
-										" "
-									)
-								}
-								TextType::Italic => {
-									rsx!(
-										i { &contents[t.span.0..t.span.1] },
-										" "
-									)
-								}
-								TextType::Plain => {
-									rsx!(
-										&contents[t.span.0..t.span.1],
-										" "
-									)
-								}
-							}
-						})
+	let _ = use_eval(cx)("renderMathInElement(document.getElementById(note-view));");
+
+    cx.render(rsx!(
+		div {
+			id: "note-view",
+			tokens.iter().map(|t| {
+				match &t.ty {
+					TokenType::H1 => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							h1 { "{sub}" }
+						)
 					}
-				)
-			}
-            TokenType::Newline => rsx!(br {}),
-            _ => rsx!(
-                b { "Invalid token!" }
-            ),
-        }
-    })))
+					TokenType::H2 => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							h2 { "{sub}" }
+						)
+					}
+					TokenType::H3 => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							h3 { "{sub}" }
+						)
+					}
+					TokenType::H4 => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							h4 { "{sub}" }
+						)
+					}
+					TokenType::H5 => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							h5 { "{sub}" }
+						)
+					}
+					TokenType::Bold => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							b { "{sub}" }
+						)
+					}
+					TokenType::Italic => {
+						let sub: &'a str = &contents[t.span.0..t.span.1];
+						rsx!(
+							i { "{sub}" }
+						)
+					}
+					TokenType::Inline(tokens) => {
+						info!("Found inline token");
+						rsx!(
+							p {
+								tokens.iter().map(|t| {
+									match t.ty {
+										TextType::Bold => {
+											rsx!(
+												b { &contents[t.span.0..t.span.1] },
+												" "
+											)
+										}
+										TextType::Italic => {
+											rsx!(
+												i { &contents[t.span.0..t.span.1] },
+												" "
+											)
+										}
+										TextType::Plain => {
+											rsx!(
+												&contents[t.span.0..t.span.1],
+												" "
+											)
+										}
+									}
+								})
+							}
+						)
+					}
+					TokenType::Newline => rsx!(br {}),
+					_ => rsx!(
+						b { "Invalid token!" }
+					),
+				}
+    })}))
 }
