0f70b1c74ef38a5a44140a6db000b036432b8221
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-23 10:36:24 -0400

    Route all paths to created handlers

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,31 @@
 //! The Volcano Markdown Viewer server. This server exposes
 //! a port on localhost which the web UI (hosted on the same machine)
 //! communicates with to fetch and display notes.
+ 
+#![feature(fs_try_exists)]
+mod handlers;
+use axum::{
+	routing::get,
+	Router
+};
+use std::fs;
 
-fn main() {
-    println!("Hello, world!");
+const LISTEN_ON: &str = "0.0.0.0:7186";
+
+
+#[tokio::main]
+async fn main() {
+	if !fs::try_exists("notes/").unwrap() {
+		fs::create_dir("notes/").unwrap();
+	}
+
+	let app = Router::new()
+		.route("/ping", get(handlers::ping))
+		.route("/get_names", get(handlers::get_note_names))
+		.route("/get_note", get(handlers::get_note_content));
+
+	axum::Server::bind(&LISTEN_ON.parse().unwrap())
+		.serve(app.into_make_service())
+		.await
+		.unwrap();
 }
