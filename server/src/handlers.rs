48cfb63f90d460fb6a01cef68f7d6a5870fadacb
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-23 10:36:14 -0400

    Add basic handlers for note listing and fetching note content

---
+++ server/src/handlers.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use axum::{Json, http::StatusCode};
+
+pub async fn ping() -> &'static str {
+	"Up!"
+}
+
+pub async fn get_note_names() -> Json<Vec<String>> {
+	Json(fs::read_dir("notes/").unwrap()
+		.filter_map(|f| {
+			let name = f.unwrap().file_name().into_string().unwrap();
+			if name.ends_with(".md") {
+				Some(name.replace(".md", ""))
+			} else {
+				None
+			}
+		})
+		.collect::<Vec<String>>())
+}
+
+pub async fn get_note_content(Json(payload): Json<serde_json::Value>) -> Result<String, StatusCode> {
+	if let serde_json::Value::String(s) = payload {
+		if fs::try_exists(format!("notes/{s}.md")).unwrap() {
+			Ok(fs::read_to_string(format!("notes/{s}.md")).unwrap())
+		} else {
+			Err(StatusCode::INTERNAL_SERVER_ERROR)
+		}
+	} else {
+		Err(StatusCode::BAD_REQUEST)
+	}
+}
