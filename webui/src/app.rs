d173dabe7ef6d0e65769eeea8a8dad8f1030c0f3
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-31 16:05:54 -0400

    Use futures to get a list of note names from the server

--- webui/src/app.rs
+++ webui/src/app.rs
@@ -1,5 +1,7 @@
 use crate::{components::note_view::NoteView, get_notes};
 use dioxus::prelude::*;
+use reqwest_wasm as reqwest;
+use std::future::Future;
 
 const TEST_FILE: &str = r#"
 # Welcome!
@@ -23,25 +25,35 @@ $$f'(x)=\lim_{\Delta x \to 0} \frac{f(x+\Delta x)-f(x)}{\Delta x}$$
 "#;
 
 pub fn app(cx: Scope) -> Element {
-	let note_names: Vec<&str> = get_notes::get_note_list();
-	let note_raw = use_state(cx, || TEST_FILE.to_string());
+    let note_raw = use_state(cx, || TEST_FILE.to_string());
+    let note_names = use_future(cx, (), |_| async move {
+        reqwest::get("http://127.0.0.1:7186/get_names")
+            .await
+            .unwrap()
+            .json::<Vec<String>>()
+            .await
+            .unwrap()
+    });
 
     cx.render(rsx!(
         style { include_str!("./css/index.css") }
-		div {
-			class: "sidebar",
-			note_names.clone().into_iter().map(|s| rsx!(
-				button {
-					class: "notebutton",
-					onclick: move |_| {
-						note_raw.set(crate::get_notes::get_note_content(s));
-					},
-					"{s}"
-				}
-			))
-		}
-        NoteView {
-			contents: "{note_raw}"
-		}
+        div { class: "sidebar",
+            match note_names.value() {
+                Some(v) => {
+                    rsx!(
+                    v.iter().map(|s| rsx!(
+                        button {
+                            class: "notebutton",
+                            onclick: move |_| {
+                                note_raw.set(crate::get_notes::get_note_content(&s));
+                            },
+                            "{s}"
+                        }
+                    )))
+                },
+                None => {rsx!(p { "Loading notes..." })}
+            }
+        }
+        NoteView { contents: "{note_raw}" }
     ))
 }
