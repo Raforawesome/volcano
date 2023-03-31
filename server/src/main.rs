b2e1079b2ff0f14a4f053671461b70aadcbd8b5c
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-03-31 16:05:28 -0400

    Allow all CORS origin URLs for GET requests

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,31 +1,33 @@
 //! The Volcano Markdown Viewer server. This server exposes
 //! a port on localhost which the web UI (hosted on the same machine)
 //! communicates with to fetch and display notes.
- 
+
 #![feature(fs_try_exists)]
 mod handlers;
-use axum::{
-	routing::get,
-	Router
-};
+use axum::{http::Method, routing::get, Router};
 use std::fs;
+use tower_http::cors::{Any, CorsLayer};
 
 const LISTEN_ON: &str = "0.0.0.0:7186";
 
-
 #[tokio::main]
 async fn main() {
-	if !fs::try_exists("notes/").unwrap() {
-		fs::create_dir("notes/").unwrap();
-	}
+    if !fs::try_exists("notes/").unwrap() {
+        fs::create_dir("notes/").unwrap();
+    }
 
-	let app = Router::new()
-		.route("/ping", get(handlers::ping))
-		.route("/get_names", get(handlers::get_note_names))
-		.route("/get_note", get(handlers::get_note_content));
+    let cors: CorsLayer = CorsLayer::new()
+        .allow_methods(vec![Method::GET])
+        .allow_origin(Any);
 
-	axum::Server::bind(&LISTEN_ON.parse().unwrap())
-		.serve(app.into_make_service())
-		.await
-		.unwrap();
+    let app = Router::new()
+        .route("/ping", get(handlers::ping))
+        .route("/get_names", get(handlers::get_note_names))
+        .route("/get_note", get(handlers::get_note_content))
+        .layer(cors);
+
+    axum::Server::bind(&LISTEN_ON.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
