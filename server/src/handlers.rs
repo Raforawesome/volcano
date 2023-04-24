29460c72fa21d0afb27eb7192439c813583e9e73
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-04-24 10:13:29 -0400

    Use header to fetch note instead of body on GET request

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, Json};
+use axum::{http::{StatusCode, HeaderMap}, Json};
 use std::fs;
 
 pub async fn ping() -> &'static str {
@@ -22,12 +22,15 @@ pub async fn get_note_names() -> Json<Vec<String>> {
 }
 
 pub async fn get_note_content(
-    Json(payload): Json<serde_json::Value>,
+    // Json(payload): Json<serde_json::Value>,
+	headers: HeaderMap
 ) -> Result<String, StatusCode> {
-    if let serde_json::Value::String(s) = payload {
+    if headers.contains_key("note") {
+		let s: &str = headers.get("note").unwrap().to_str().map_err(|_| StatusCode::BAD_REQUEST)?;
         if fs::try_exists(format!("notes/{s}.md")).unwrap() {
             Ok(fs::read_to_string(format!("notes/{s}.md")).unwrap())
         } else {
+			println!("Didn't find requested note '{s}.md'");
             Err(StatusCode::INTERNAL_SERVER_ERROR)
         }
     } else {
