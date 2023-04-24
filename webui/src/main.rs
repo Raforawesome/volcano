f3969014409c13c59caa75c7855b2ca408bfd692
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-04-24 10:12:31 -0400

    Make `webui` not run on non-wasm platforms

--- webui/src/main.rs
+++ webui/src/main.rs
@@ -5,11 +5,18 @@
 //! normal web features like web requests, but not things like manipulate
 //! the filesystem of the server on which it's hosted.
 
+#![feature(lazy_cell)]
 mod app;
 mod components;
 mod get_notes;
 
+#[cfg(target_arch = "wasm32")]
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     dioxus_web::launch(app::app);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    println!("INCOMPATIBLE PLATFORM: This crate is only compatible with WASM.");
 }
